ba6cfad87bce3ea896c9fbc02d2a49b570944bb3
DADIxodi <tipodosym@gmail.com>
2024-03-24 10:25:56 +0500

    lab7 full

---
+++ lab7/Task1/informatics/e_tasks/A.py
@@ -0,0 +1,5 @@
+def min4 (a, b, c, d):
+    print(min(a, b, c, d))
+
+a = list(map(int, input().split()))
+min4(a[0], a[1], a[2], a[3])
---
+++ lab7/Task1/informatics/e_tasks/B.py
@@ -0,0 +1,5 @@
+def power2 (a, b):
+    print(a**b)
+
+a = list(map(int, input().split()))
+power2(a[0], a[1])
